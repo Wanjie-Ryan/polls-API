b59010bdc0f2d7693a88667f0ad1e51025f755a1
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2025-02-24 15:25:28 +0300

    Polls Api

--- src/main/java/com/voting/votingapp/controller/PollController.java
+++ src/main/java/com/voting/votingapp/controller/PollController.java
@@ -1,7 +1,9 @@
 package com.voting.votingapp.controller;
 
+import com.voting.votingapp.dto.Vote;
 import com.voting.votingapp.model.Poll;
 import com.voting.votingapp.service.PollService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,19 +17,33 @@ public class PollController {
 
     @Autowired
     private PollService pollService;
+
     @PostMapping("/create")
-    public ResponseEntity<Poll> createPoll(@RequestBody Poll poll){
+    public ResponseEntity<Poll> createPoll(@RequestBody Poll poll) {
 
         Poll newPoll = pollService.createPoll(poll);
         return new ResponseEntity<>(newPoll, HttpStatus.CREATED);
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Poll>> getAllPolls(){
+    public ResponseEntity<List<Poll>> getAllPolls() {
 
         List<Poll> getPolls = pollService.getAllPolls();
         return new ResponseEntity<>(getPolls, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}/poll")
+
+    public ResponseEntity<Poll> getPollById(@PathVariable Long id) {
+
+        Poll getSinglePoll = pollService.getPollById(id).orElseThrow(() -> new EntityNotFoundException("Poll with id: " + id + " not found"));
+        return new ResponseEntity<>(getSinglePoll, HttpStatus.OK);
+    }
+
+    //voting functionality
+    @PostMapping("/vote")
+    public void vote(@RequestBody Vote vote) {
+        pollService.vote(vote.getPollId(), vote.getOptionIndex());
+    }
 
 }
--- src/main/java/com/voting/votingapp/service/PollService.java
+++ src/main/java/com/voting/votingapp/service/PollService.java
@@ -1,13 +1,16 @@
 package com.voting.votingapp.service;
 
+import com.voting.votingapp.model.OptionVote;
 import com.voting.votingapp.model.Poll;
 import com.voting.votingapp.repository.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PollService {
@@ -25,8 +28,35 @@ public class PollService {
     }
 
     // METHOD FOR GETTING ALL POLLS
-    public List<Poll> getAllPolls(){
+    public List<Poll> getAllPolls() {
         return pollRepository.findAll();
     }
 
+    //METHOD FOR GETTING A SINGLE POLL
+    public Optional<Poll> getPollById(Long id) {
+
+        Optional<Poll> getSinglePoll = pollRepository.findById(id);
+        return getSinglePoll;
+
+    }
+
+    public void vote(Long pollId, int optionIndex) {
+
+        // 1. get entire poll from db
+        Optional<Poll> specificPoll = getPollById(pollId);
+        // 2. get all options
+        List<OptionVote> options = specificPoll.get().getOptions();
+        // 3. if index for vote is invalid, throw exception
+        if (optionIndex < 0 || optionIndex >= options.size()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid option index");
+        }
+        // 4. get selected option
+        OptionVote selectedOption = options.get(optionIndex);
+        // 5. increment vote count for selected option
+        selectedOption.setVoteCount(selectedOption.getVoteCount() + 1);
+        // 6. save the incremented vote option to the DB
+        pollRepository.save(specificPoll.get());
+
+
+    }
 }
